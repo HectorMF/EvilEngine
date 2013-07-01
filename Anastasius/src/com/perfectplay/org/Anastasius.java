43412e0f4bec05ff2de97b7ac5d5dcb911e190da
Hector Medina-Fetterman <javi@medina.net>
2013-07-01 13:36:07 -0400

    physics simulation using box2d and lights from box2dlights

--- Anastasius/src/com/perfectplay/org/Anastasius.java
+++ Anastasius/src/com/perfectplay/org/Anastasius.java
@@ -2,16 +2,29 @@ package com.perfectplay.org;
 
 import java.util.ArrayList;
 
+import box2dLight.ConeLight;
+import box2dLight.PointLight;
+import box2dLight.RayHandler;
+
 import com.artemis.Entity;
 import com.artemis.EntitySystem;
-import com.artemis.World;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.perfectplay.org.components.SpriteRender;
 import com.perfectplay.org.components.Transform;
 import com.perfectplay.org.graphics.AnimatedSprite;
@@ -23,7 +36,7 @@ public class Anastasius implements ApplicationListener {
 	
 	//artemis entity system stuffs
 	private World world;
-	private EntitySystem renderSystem;
+	//private EntitySystem renderSystem;
 	
 	
 	private OrthographicCamera camera;
@@ -32,16 +45,60 @@ public class Anastasius implements ApplicationListener {
 	//textures
 	Texture texture;
 	Texture texture2;
-	
+	private RayHandler rayHandler;
+	private Box2DDebugRenderer render;
+	Body circleBody;
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		float w = Gdx.graphics.getWidth()/5;
+		float h = Gdx.graphics.getHeight()/5;
 		
+
 		camera = new OrthographicCamera(w, h);      
         camera.position.set(w / 2, h/ 2, 0);
-        
+        camera.update();
 		batch = new SpriteBatch();
+		
+		world = new World(new Vector2(0,-9.8f),false);
+		
+		
+		
+		render = new Box2DDebugRenderer();
+		
+		BodyDef circle = new BodyDef();
+		
+		circle.type = BodyType.DynamicBody;
+		circle.position.set(w/2, h/2-10);
+		circleBody = world.createBody(circle);
+		
+		CircleShape circleShape = new CircleShape();
+		circleShape.setRadius(5f);
+		
+		FixtureDef circleFixture = new FixtureDef();
+		
+		circleFixture.shape = circleShape;
+		circleFixture.density = 0.5f;
+		circleFixture.friction = 0.2f;
+		circleFixture.restitution = 1f;
+		
+		circleBody.createFixture(circleFixture);
+		
+		
+		BodyDef groundDef = new BodyDef();
+		groundDef.position.set(0, 3);
+		
+		Body groundBody = world.createBody(groundDef);
+		
+		PolygonShape box = new PolygonShape();
+		box.setAsBox(camera.viewportWidth * 2, 3.0f);
+		
+		groundBody.createFixture(box,0f);
+		
+		
+		
+		
+		
+		
 		
 		//load in some textures from the assets/data folder
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
@@ -51,8 +108,13 @@ public class Anastasius implements ApplicationListener {
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
 		
- 
+		rayHandler = new RayHandler(world);
+		rayHandler.setCombinedMatrix(camera.combined);
 		
+		new PointLight(rayHandler,1000,Color.WHITE,150,w/2,h/2);
+		new PointLight(rayHandler,200,Color.RED,50,w/2-10,h/2+50);
+		new ConeLight(rayHandler,1000,Color.BLUE,200,w/2+50,h/2-50,110,40);
+		/*
 		//artemis stuffs
 		world = new World();
 		renderSystem = world.setSystem(new SpriteRenderSystem(batch), true);
@@ -69,7 +131,7 @@ public class Anastasius implements ApplicationListener {
 		
 		e.addComponent(new SpriteRender(aSprite));
 		e.addToWorld();
-
+*/
 		
 	}
 
@@ -78,21 +140,29 @@ public class Anastasius implements ApplicationListener {
 		batch.dispose();
 		texture.dispose();
 		texture2.dispose();
+		rayHandler.dispose();
+		world.dispose();
 	}
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(0, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		camera.update();
-		batch.setProjectionMatrix(camera.combined);
-		
+		//batch.setProjectionMatrix(camera.combined);
+		/*
 		world.setDelta(Gdx.graphics.getDeltaTime());
 		world.process();
 		
 		batch.begin();
 		renderSystem.process();
 		batch.end();
+		*/
+		render.render(world,camera.combined);
+		rayHandler.updateAndRender();
+		world.step(1/60f,6,2);
+		
+		
 	}
 
 	@Override
