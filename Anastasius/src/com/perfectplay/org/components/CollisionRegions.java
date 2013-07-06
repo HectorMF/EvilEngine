cee0748a94a78a6b26043963469efd61e7808109
Hector Medina-Fetterman <javi@medina.net>
2013-07-05 22:07:57 -0400

    updated CollisionRegion

--- Anastasius/src/com/perfectplay/org/components/CollisionRegions.java
+++ Anastasius/src/com/perfectplay/org/components/CollisionRegions.java
@@ -4,47 +4,62 @@ import com.artemis.Component;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.perfectplay.org.events.CollisionEvent;
 import com.perfectplay.org.systems.PhysicsSystem;
 
 public class CollisionRegions extends Component {
 	private Body regions;
 	public CollisionRegions(){
-		BodyDef bDef = new BodyDef();
-		bDef.type = BodyType.DynamicBody;
-		Body bod  = PhysicsSystem.createBody(bDef);
-		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(1f, 0.1f);
-		bod.createFixture(shape,0.1f);
-
-		shape.setAsBox(0.1f, 1f,new Vector2(-1f + 0.1f, 1f), 0);
-		bod.createFixture(shape, 0.1f);
-		
-		shape.setAsBox(0.1f, 1f, new Vector2(1f - 0.1f, 1f), 0);
-		bod.createFixture(shape, 0.1f);
+		BodyDef bodDef = new BodyDef();
+		bodDef.type = BodyType.DynamicBody;
+		regions = PhysicsSystem.createBody(bodDef);
 	}
 	
-	public static void AddPolygonRegion(Vector2[] vertices){
+	public void addRegion(FixtureDef region, CollisionEvent event){
+		Fixture fixture = regions.createFixture(region);
+		fixture.setUserData(event);
+	}
+	
+	public static FixtureDef createPolygonRegion(Vector2[] vertices, short category, short group, short mask){
 		PolygonShape shape = new PolygonShape();
 		shape.set(vertices);
 		FixtureDef fixtureDef = new FixtureDef();
 		fixtureDef.shape = shape;
 		fixtureDef.isSensor = true;
-		//fixtureDef.filter.
+		fixtureDef.filter.categoryBits = category;
+		fixtureDef.filter.groupIndex = group;
+		fixtureDef.filter.maskBits = mask;
+		return fixtureDef;
 	}
 	
-	public static void AddCircleRegion(float radius, Vector2 position){
+	public static FixtureDef createCircleRegion(float radius, Vector2 position, short category, short group, short mask){
 		CircleShape shape = new CircleShape();
 		shape.setRadius(radius);
 		shape.setPosition(position);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.isSensor = true;
+		fixtureDef.filter.categoryBits = category;
+		fixtureDef.filter.groupIndex = group;
+		fixtureDef.filter.maskBits = mask;
+		return fixtureDef;
 	}
 	
-	public static void AddBoxRegion(float width, float height, Vector2 position, float angle){
+	public static FixtureDef createBoxRegion(float width, float height, Vector2 position, float angle, short category, short group, short mask){
 		PolygonShape shape = new PolygonShape();
 		shape.setAsBox(width, height, position, angle);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.isSensor = true;
+		fixtureDef.filter.categoryBits = category;
+		fixtureDef.filter.groupIndex = group;
+		fixtureDef.filter.maskBits = mask;
+		return fixtureDef;
 	}
 	
 	public Body getBody(){
